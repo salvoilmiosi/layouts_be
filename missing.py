50d4b4888e53a35d4433ddfedbf9f112caeeac76
Salvo Miosi <salvo.ilmiosi@gmail.com>
2021-08-19 01:00:31 +0200

    aggiunto script per controllare mesi mancanti

---
+++ missing.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import json
+import os
+from pathlib import Path
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+input_directory = Path(__file__).parent / 'work/letture'
+
+clienti = {}
+
+for f in input_directory.glob('*.json'):
+    with open(f, 'r') as file:
+        data = json.load(file)
+
+    obj_cliente = {}
+    for obj in data:
+        if not 'values' in obj: continue
+        for v in obj['values']:
+            pod = v['codice_pod']
+            if pod not in obj_cliente:
+                obj_cliente[pod] = []
+            obj_cliente[pod].append(date.fromisoformat(v['mese_fattura']))
+    clienti[f.name] = obj_cliente
+
+check_date = date.today()
+check_date = date(check_date.year, check_date.month, 1)
+
+for _ in range(10):
+    check_date -= relativedelta(months=1)
+    not_found = 0
+    for nome,cliente in clienti.items():
+        for pod,mesi in cliente.items():
+            if check_date not in mesi:
+                not_found += 1
+                print(nome, pod, check_date)
+    if not_found == 0: break
